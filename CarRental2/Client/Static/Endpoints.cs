4eb5cf8210be06c7a8167b593fe4dc580d103a5b
RaynerKee <2204563G@student.tp.edu.sg>
2024-02-04 22:13:35 +0800

    Added Blazor List Pages with Delete function

---
+++ CarRental2/Client/Static/Endpoints.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace CarRental2.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string MakesEndpoint = $"{Prefix}/makes";
+        public static readonly string ModelsEndpoint = $"{Prefix}/models";
+        public static readonly string ColoursEndpoint = $"{Prefix}/colours";
+        public static readonly string VehiclesEndpoint = $"{Prefix}/vehicles";
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string BookingsEndpoint = $"{Prefix}/bookings";
+        public static readonly string SavingsEndpoint = $"{Prefix}/saving";
+        public static readonly string ExpensesEndpoint = $"{Prefix}/expense";
+        public static readonly string ArticlesEndpoint = $"{Prefix}/article";
+    }
+}
+
